87d7c4c0a70579db990f7637cb279518678d39aa
Ido <idoben2@hotmail.com>
2023-12-31 11:51:31 -0800

    Get rid of unused set methods on WinchCable

--- src/main/java/frc/robot/simulation/winch/WinchCable.java
+++ src/main/java/frc/robot/simulation/winch/WinchCable.java
@@ -46,23 +46,8 @@ public class WinchCable {
     return m_unspooledLenMeters;
   }
 
-  /**
-   * Sets the length of the cable that is unspooled.
-   */
-  public void setUnspooledLenMeters(double unspooledLenMeters) {
-    if (unspooledLenMeters < 0 || unspooledLenMeters > m_totalLenMeters) {
-      throw new IllegalArgumentException("unspooledLenMeters must be between 0 and totalLenMeters");
-    }
-
-    m_unspooledLenMeters = unspooledLenMeters;
-  }
-
   public WindingOrientation getWindingOrientation() {
     return m_windingOrientation;
-  }
-
-  public void setWindingOrientation(WindingOrientation windingOrientation) {
-    m_windingOrientation = windingOrientation;
   }
 
   public double calcSpooledLenMeters() {

bc4c7730eef1febbfa9aae6f7fb26d3509a9d71d
Ido <idoben2@hotmail.com>
2024-01-01 10:54:11 -0800

    Factory was incorrectly using Java hashmap instead of PrefixedMap

--- src/main/java/frc/robot/shuffle/SupplierMapFactory.java
+++ src/main/java/frc/robot/shuffle/SupplierMapFactory.java
@@ -1,24 +1,34 @@
 package frc.robot.shuffle;
 
-import java.util.concurrent.ConcurrentHashMap;
+import frc.robot.shuffle.PrefixedConcurrentMap.Client;
 import java.util.function.Supplier;
 
 /**
- * This class is a factory for the global singleton instance of ConcurrentHashMap.
+ * This class is a factory for the global singleton instance of PrefixedConcurrentMap.
  */
 public class SupplierMapFactory {
   /**
    * This is the global singleton instance of ConcurrentHashMap.
    */
   @SuppressWarnings("LineLengthCheck")
-  private static final ConcurrentHashMap<String, Supplier<MultiType>> globalMap = new ConcurrentHashMap<>();
+  private static final PrefixedConcurrentMap<Supplier<MultiType>> globalMap = new PrefixedConcurrentMap<>();
 
   private SupplierMapFactory() {
     // Private constructor to prevent instantiation
   }
 
   // Method to get the global singleton instance of ConcurrentHashMap
-  public static ConcurrentHashMap<String, Supplier<MultiType>> getGlobalInstance() {
+  public static PrefixedConcurrentMap<Supplier<MultiType>> getGlobalInstance() {
     return globalMap;
+  }
+
+  /**
+   * Returns a client for the global map with the specified prefix.
+   * In Simulation, all SupplierMaps are prefixed with "sim".
+   */
+  public static Client<Supplier<MultiType>> getGlobalSimClient() {
+    PrefixedConcurrentMap<Supplier<MultiType>> globalMap = SupplierMapFactory.getGlobalInstance();
+
+    return globalMap.getClientWithPrefix("sim");
   }
 }
--- src/test/java/frc/robot/shuffle/SupplierMapFactoryTest.java
+++ src/test/java/frc/robot/shuffle/SupplierMapFactoryTest.java
@@ -3,7 +3,6 @@ package frc.robot.shuffle;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,15 +12,15 @@ class SupplierMapFactoryTest {
   @Test
   @DisplayName("Test that getGlobalInstance returns a ConcurrentHashMap instance")
   void testGetGlobalInstanceReturnsMap() {
-    ConcurrentHashMap<String, Supplier<MultiType>> map = SupplierMapFactory.getGlobalInstance();
+    PrefixedConcurrentMap<Supplier<MultiType>> map = SupplierMapFactory.getGlobalInstance();
     assertNotNull(map, "getGlobalInstance should return a non-null ConcurrentHashMap instance.");
   }
 
   @Test
   @DisplayName("Test that getGlobalInstance always returns the same ConcurrentHashMap instance")
   void testGetGlobalInstanceSingletonProperty() {
-    ConcurrentHashMap<String, Supplier<MultiType>> map1 = SupplierMapFactory.getGlobalInstance();
-    ConcurrentHashMap<String, Supplier<MultiType>> map2 = SupplierMapFactory.getGlobalInstance();
+    PrefixedConcurrentMap<Supplier<MultiType>> map1 = SupplierMapFactory.getGlobalInstance();
+    PrefixedConcurrentMap<Supplier<MultiType>> map2 = SupplierMapFactory.getGlobalInstance();
 
     assertSame(map1,
         map2,

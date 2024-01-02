6a169c7e2e49091db765808ba5e6318a777ce200
Ido <idoben2@hotmail.com>
2024-01-01 18:05:53 -0800

    Unit tests to test shuffleboard client

---
+++ src/test/java/frc/robot/shuffle/ShuffleClientTest.java
@@ -0,0 +1,67 @@
+package frc.robot.shuffle;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import frc.robot.simulation.framework.SimManager;
+import frc.robot.simulation.sample.SampleSimModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test querying each sim model for the properties it shows on the Shuffleboard dash.
+ */
+public class ShuffleClientTest {
+
+  @BeforeEach
+  public void setUp() {
+    // $TODO - Reset the global cache of dashboard items
+  }
+
+  @Test
+  public void creatingSimManagerWithNullShuffleClientShouldSucceed() {
+    int ratio = 2;
+
+    SimManager<Integer, Integer> sampleSimManager = new SimManager<Integer, Integer>(
+        new SampleSimModel(ratio), null, () -> true);
+    assertTrue(sampleSimManager != null);
+  }
+
+  // $TODO - Get rid of this
+  // public int testSimulation_Helper(int ratio,
+  // int initialOutputValue,
+  // int inputValue,
+  // boolean isEnabledDuringInit,
+  // boolean isEnabledDuringSimulation,
+  // boolean doSimulationTenTimes) {
+
+  // final int[] inputVariable = {
+  // inputValue
+  // };
+
+  // final int[] outputVariable = {
+  // initialOutputValue
+  // };
+
+  // final boolean[] isRobotEnabled = {
+  // isEnabledDuringInit
+  // };
+
+  // SimManager<Integer, Integer> sampleSimManager = new SimManager<Integer, Integer>(
+  // new SampleSimModel(ratio), null, () -> isRobotEnabled[0]);
+
+  // sampleSimManager.setInputHandler(new LambdaSimInput<Integer>(() -> {
+  // return inputVariable[0];
+  // }));
+
+  // sampleSimManager.setOutputHandler(new LambdaSimOutput<Integer>((numOutput) -> {
+  // outputVariable[0] = numOutput;
+  // }));
+
+  // isRobotEnabled[0] = isEnabledDuringSimulation;
+  // if (doSimulationTenTimes) {
+  // runSimulationTenTimes(sampleSimManager);
+  // }
+
+  // return outputVariable[0];
+  // }
+}

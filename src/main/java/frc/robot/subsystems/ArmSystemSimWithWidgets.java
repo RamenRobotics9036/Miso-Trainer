e492387123e1061381163456a9b18ccbe941a3bd
Ido <idoben2@hotmail.com>
2024-01-13 19:06:13 -0800

    Hold old widgets

--- src/main/java/frc/robot/subsystems/ArmSystemSimWithWidgets.java
+++ src/main/java/frc/robot/subsystems/ArmSystemSimWithWidgets.java
@@ -90,23 +90,25 @@ public class ArmSystemSimWithWidgets extends ArmSystemSim {
 
   private void addShuffleboardExtenderList() {
     // Extender functional
-    Widget pos = m_defaultLayout.getWidgetPosition("Extender Functional");
-    Shuffleboard.getTab("Simulation")
-        .addBoolean("Extender Functional", () -> !m_extenderSimulation.getIsBroken())
-        .withWidget(BuiltInWidgets.kBooleanBox)
-        .withProperties(Map.of("colorWhenTrue", "#C0FBC0", "colorWhenFalse", "#8B0000"))
-        .withPosition(pos.x, pos.y).withSize(pos.width, pos.height);
+    // $TODO - Hiding old widget for now
+    // Widget pos = m_defaultLayout.getWidgetPosition("Extender Functional");
+    // Shuffleboard.getTab("Simulation")
+    // .addBoolean("Extender Functional", () -> !m_extenderSimulation.getIsBroken())
+    // .withWidget(BuiltInWidgets.kBooleanBox)
+    // .withProperties(Map.of("colorWhenTrue", "#C0FBC0", "colorWhenFalse", "#8B0000"))
+    // .withPosition(pos.x, pos.y).withSize(pos.width, pos.height);
 
     // Extender percent extended
-    pos = m_defaultLayout.getWidgetPosition("Extender % Extended");
-    Shuffleboard.getTab("Simulation")
-        .addDouble("Extender % Extended", () -> m_extenderSimulation.getExtendedPercent())
-        .withWidget(BuiltInWidgets.kNumberBar)
-        .withProperties(Map.of("min", 0.0, "max", 1.0, "show text", false))
-        .withPosition(pos.x, pos.y).withSize(pos.width, pos.height);
+    // $TODO - Hiding old widget for now
+    // pos = m_defaultLayout.getWidgetPosition("Extender % Extended");
+    // Shuffleboard.getTab("Simulation")
+    // .addDouble("Extender % Extended", () -> m_extenderSimulation.getExtendedPercent())
+    // .withWidget(BuiltInWidgets.kNumberBar)
+    // .withProperties(Map.of("min", 0.0, "max", 1.0, "show text", false))
+    // .withPosition(pos.x, pos.y).withSize(pos.width, pos.height);
 
     // Extender sensor display
-    pos = m_defaultLayout.getWidgetPosition("Extender Sensor");
+    Widget pos = m_defaultLayout.getWidgetPosition("Extender Sensor");
     Shuffleboard.getTab("Simulation").addBoolean("Extender Sensor", () -> !m_sensorSim.getValue())
         .withWidget(BuiltInWidgets.kBooleanBox)
         .withProperties(Map.of("colorWhenTrue", "#C0FBC0", "colorWhenFalse", "#FFFFFF"))

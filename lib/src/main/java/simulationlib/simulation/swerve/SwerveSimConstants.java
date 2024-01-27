e200bca93d0dabd38c4560add7c2a3149c18ec70
Ido <idoben2@hotmail.com>
2024-01-27 14:49:51 -0800

    Swerve motors we create in sim are in 40+ range to avoid collissions

--- lib/src/main/java/simulationlib/simulation/swerve/SwerveSimConstants.java
+++ lib/src/main/java/simulationlib/simulation/swerve/SwerveSimConstants.java
@@ -38,19 +38,19 @@ public final class SwerveSimConstants {
   public static final class Can {
     public static final int pigeon = 9;
 
-    public static final int frontLeftCanCoder = 10;
-    public static final int frontRightCanCoder = 11;
-    public static final int backLeftCanCoder = 12;
-    public static final int backRightCanCoder = 13;
+    public static final int frontLeftCanCoder = 40;
+    public static final int frontRightCanCoder = 41;
+    public static final int backLeftCanCoder = 42;
+    public static final int backRightCanCoder = 43;
 
-    public static final int frontLeftDriveMotor = 20;
-    public static final int frontLeftTurnMotor = 21;
-    public static final int frontRightDriveMotor = 22;
-    public static final int frontRightTurnMotor = 23;
-    public static final int backLeftDriveMotor = 24;
-    public static final int backLeftTurnMotor = 25;
-    public static final int backRightDriveMotor = 26;
-    public static final int backRightTurnMotor = 27;
+    public static final int frontLeftDriveMotor = 40;
+    public static final int frontLeftTurnMotor = 41;
+    public static final int frontRightDriveMotor = 42;
+    public static final int frontRightTurnMotor = 43;
+    public static final int backLeftDriveMotor = 44;
+    public static final int backLeftTurnMotor = 45;
+    public static final int backRightDriveMotor = 46;
+    public static final int backRightTurnMotor = 47;
   }
 
   /**
